88943415fc3b1202d8c723fce0507f88ca420847
hemantthakkar <hthakkar275@gmail.com>
2019-04-23 22:03:49 -0500

    Initial commit

---
+++ financial-exchange-orders/src/main/java/org/hemant/thakkar/financialexchange/FinancialExchangeOrdersApplication.java
@@ -0,0 +1,14 @@
+package org.hemant.thakkar.financialexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialExchangeOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialExchangeOrdersApplication.class, args);
+	}
+
+}
+
